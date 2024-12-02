f2be64f9a47f3c0db1dd04eb4fe4ae19f4759c5d
Stan Winitsky <stan.winitsky@gmail.com>
2024-12-02 10:11:58 -0500

    Cleanup

--- src/main/webapp/src/app/api/api/api.ts
+++
@@ -1,4 +0,0 @@
-export * from './welcomeController.service';
-import { WelcomeControllerService } from './welcomeController.service';
-export * from './welcomeController.serviceInterface';
-export const APIS = [WelcomeControllerService];
